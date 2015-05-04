9368ad5f69a909235cbd3d4d21de29a8361be24b
jtkuhn <jtkuhn@foxguardsolutions.com>
2015-05-04 10:25:02 -0400

    All finished except obstacles!

--- RoverKata/Rover.cs
+++ RoverKata/Rover.cs
@@ -7,127 +7,200 @@ using System.Threading.Tasks;
 namespace RoverKata
 {
 
-    abstract class Orientation
+    abstract public class Orientation
     {
-        protected int x;
-        protected int y;
-        public abstract void MoveForward(Rover rover);
-        public abstract void MoveBackward(Rover rover);
         public abstract void TurnLeft(Rover rover);
         public abstract void TurnRight(Rover rover);
+        public abstract void MoveForward(Rover rover);
+        public abstract void MoveBackward(Rover rover);
     }
 
     class OrientationEast : Orientation
     {
-        public override void MoveForward(Rover rover)
+        public override String ToString()
         {
-            x++;
-        }
-        public override void MoveBackward(Rover rover)
-        {
-            x--;
+            return "east";
         }
         public override void TurnLeft(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationNorth());
         }
 
         public override void TurnRight(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationSouth());
+        }
+
+        public override void MoveForward(Rover rover)
+        {
+            if (rover.X == rover.XLimitHigh) rover.X = rover.XLimitLow;
+            else rover.X++;
+        }
+
+        public override void MoveBackward(Rover rover)
+        {
+            if (rover.X == rover.XLimitLow) rover.X = rover.XLimitHigh;
+            else rover.X--;
         }
     }
 
     class OrientationWest : Orientation
     {
-        public override void MoveForward(Rover rover)
+        public override String ToString()
         {
-            x--;
+            return "west";
         }
-
-        public override void MoveBackward(Rover rover)
-        {
-            x++;
-        }
-
         public override void TurnLeft(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationSouth());
         }
 
         public override void TurnRight(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationNorth());
+        }
+
+        public override void MoveForward(Rover rover)
+        {
+            if (rover.X == rover.XLimitLow) rover.X = rover.XLimitHigh;
+            else rover.X --;
+        }
+
+        public override void MoveBackward(Rover rover)
+        {
+            if (rover.X == rover.XLimitHigh) rover.X = rover.XLimitLow;
+            else rover.X++;
         }
     }
 
     class OrientationNorth : Orientation
     {
-        public override void MoveForward(Rover rover)
+        public override String ToString()
         {
-            y++;
+            return "north";
         }
-
-        public override void MoveBackward(Rover rover)
-        {
-            y--;
-        }
-
         public override void TurnLeft(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationWest());
         }
 
         public override void TurnRight(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationEast());
+        }
+
+        public override void MoveForward(Rover rover)
+        {
+            if (rover.Y == rover.YLimitHigh) rover.Y = rover.YLimitLow;
+            else rover.Y++;
+        }
+
+        public override void MoveBackward(Rover rover)
+        {
+            if (rover.Y == rover.YLimitLow) rover.Y = rover.YLimitHigh;
+            else rover.Y--;
         }
     }
 
     class OrientationSouth : Orientation
     {
-        public override void MoveForward(Rover rover)
-        {
-            y++;
-        }
 
-        public override void MoveBackward(Rover rover)
+        public override String ToString()
         {
-            y--;
+            return "south";
         }
-
         public override void TurnLeft(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationEast());
         }
 
         public override void TurnRight(Rover rover)
         {
-            throw new NotImplementedException();
+            rover.SetOrientation(new OrientationWest());
+        }
+
+        public override void MoveForward(Rover rover)
+        {
+            if (rover.Y == rover.YLimitLow) rover.Y = rover.YLimitHigh;
+            else rover.Y--;
+        }
+
+        public override void MoveBackward(Rover rover)
+        {
+            if (rover.Y == rover.YLimitHigh) rover.Y = rover.YLimitLow;
+            else rover.Y++;
         }
     }
 
 
     public class Rover
     {
-        private Orientation orientation = new OrientationEast();
-        private int x;
-        private int y;
+        private Orientation orientation;
+        private int _x;
+        private int _y;
+        public int XLimitHigh = 5;
+        public int XLimitLow = -5;
+        public int YLimitHigh = 5;
+        public int YLimitLow = -5;
 
         public Rover(int x, int y)
         {
-            this.x = x;
-            this.y = y;
+            orientation = new OrientationNorth();
+            _x = x;
+            _y = y;
         }
 
-        public int GetX()
+
+
+        public void ReadCommands(String s)
         {
-            return x;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == 'f') MoveForward();
+                else if (c == 'b') MoveBackward();
+                else if (c == 'r') TurnRight();
+                else if (c == 'l') TurnLeft();
+            }
         }
 
-        public int GetY()
+        public void SetOrientation(Orientation o)
         {
-            return y;
+            orientation = o;
+        }
+        public String GetOrientation()
+        {
+            return orientation.ToString();
+        }
+
+        public int X
+        {
+            get { return _x;}
+            set { _x = value;}
+        }
+
+        public int Y    
+        { 
+            get { return _y; } 
+            set { _y = value; } 
+        }
+
+        public void MoveForward()
+        {
+            orientation.MoveForward(this);
+        }
+
+        public void MoveBackward()
+        {
+            orientation.MoveBackward(this);
+        }
+        public void TurnLeft()
+        {
+            orientation.TurnLeft(this);
+        }
+        public void TurnRight()
+        {
+            orientation.TurnRight(this);
         }
 
     }
--- UnitTestProject1/RoverTest.cs
+++ UnitTestProject1/RoverTest.cs
@@ -19,11 +19,93 @@ namespace UnitTestProject1
         [Test]
         public void TestConstructor()
         {
-            Assert.AreEqual(1, rover.GetX());
-            Assert.AreEqual(1, rover.GetY());
+            Assert.AreEqual(1, rover.X);
+            Assert.AreEqual(1, rover.Y);
         }
-        
 
-        
+        [Test]
+        public void TestOrientationStart()
+        {
+            Assert.AreEqual("north", rover.GetOrientation());
+        }
+
+        [Test]
+        public void TestTurnLeft()
+        {
+            rover.TurnLeft();
+            Assert.AreEqual("west", rover.GetOrientation());
+            rover.TurnLeft();
+            Assert.AreEqual("south", rover.GetOrientation());
+            rover.TurnLeft();
+            Assert.AreEqual("east", rover.GetOrientation());
+            rover.TurnLeft();
+            Assert.AreEqual("north", rover.GetOrientation());
+        }
+
+        [Test]
+        public void TestTurnRight()
+        {
+            rover.TurnRight();
+            Assert.AreEqual("east", rover.GetOrientation());
+            rover.TurnRight();
+            Assert.AreEqual("south", rover.GetOrientation());
+            rover.TurnRight();
+            Assert.AreEqual("west", rover.GetOrientation());
+            rover.TurnRight();
+            Assert.AreEqual("north", rover.GetOrientation());
+        }
+
+        [Test]
+        public void TestMoveForward()
+        {
+            rover.MoveForward();
+            Assert.AreEqual(2, rover.Y); 
+            rover.TurnLeft();
+            rover.MoveForward();
+            Assert.AreEqual(0, rover.X);
+            rover.TurnLeft();
+            rover.MoveForward();
+            Assert.AreEqual(1, rover.Y);
+            rover.TurnLeft();
+            rover.MoveForward();
+            Assert.AreEqual(1, rover.X);
+        }
+
+        [Test]
+        public void TestMoveBackward()
+        {
+            rover.MoveBackward();
+            Assert.AreEqual(0, rover.Y);
+            rover.TurnRight();
+            rover.MoveBackward();
+            Assert.AreEqual(0, rover.X);
+            rover.TurnRight();
+            rover.MoveBackward();
+            Assert.AreEqual(1, rover.Y);
+            rover.TurnRight();
+            rover.MoveBackward();
+            Assert.AreEqual(1, rover.X);
+        }
+
+        [Test]
+        public void TestReadCommands()
+        {
+            rover.ReadCommands("ffrrffflfrrbb");
+            Assert.AreEqual(0, rover.Y);
+            Assert.AreEqual(4, rover.X);
+        }
+
+        [Test]
+        public void TestWrapAround()
+        {
+            rover.ReadCommands("fffffffffff");
+            Assert.AreEqual(1, rover.Y);
+            rover.ReadCommands("bbbbbbbbbbb");
+            Assert.AreEqual(1, rover.Y);
+            rover.ReadCommands("rfffffffffff");
+            Assert.AreEqual(1, rover.X);
+            rover.ReadCommands("bbbbbbbbbbb");
+            Assert.AreEqual(1, rover.X);
+        }
     }
 }
